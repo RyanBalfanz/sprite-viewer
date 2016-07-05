5da1bba97ef7757d1184b587c55880f1ac0816fa
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-07-04 18:00:08 -0700

    Support user given rotation speed

--- app.js
+++ app.js
@@ -13,18 +13,40 @@
 		var ctx = canvas.getContext("2d");
 		var img = document.getElementById("viewer-img-source");
 
-		var frameCount = 40;
-		var frameWidth = 8320 / frameCount;
-		var frameHeight = 432;
-		var frameRate = 60;
+		var frameCount = get_config_by_key("frameCount");
+		var frameWidth = get_config_by_key("frameWidth");
+		var frameHeight = get_config_by_key("frameHeight");
 
 		var frame = 0;
-		setInterval(function(){
-			render(frame % frameCount);
-			frame++;
-		}, 1000/frameRate);
+		var theta = 0;
+		var t0 = new Date();
+		var ti = t0;
+		var tf = t0;
 
-		function render(frame) {
+		function loop() {
+			update();
+			render_frame(frame);
+			requestAnimationFrame(loop);
+		}
+
+		requestAnimationFrame(loop);
+
+		function update() {
+			var dt = tf - ti;
+			var omega = get_config_by_key("omega");
+			theta += omega * (dt / 1000);
+			theta = theta % 1.0;
+			frame = Math.round(theta * frameCount);
+			frame = frame % frameCount;
+			ti = tf;
+			tf = new Date();
+
+			var FPS = 1.0 / (dt / 1000);
+			// console.log("FPS:", Math.round(FPS));
+			// console.log(theta, frame);
+		}
+
+		function render_frame(frame) {
 			var sx = frame * frameWidth;
 			var sy = 0;
 			var sWidth = frameWidth;
@@ -40,5 +62,24 @@
 				dx, dy, dWidth, dHeight
 			);
 		}
+	}
+
+	function get_form_by_id(id) {
+		var form;
+		for (var i=0; i < document.forms.length; i++) {
+			var f = document.forms[i];
+			if (f.id == id) {
+				form = f;
+				break;
+			}
+		}
+		return form;
+	}
+
+	function get_config_by_key(key) {
+		var FORM_ID = "configuration";
+		var form = get_form_by_id(FORM_ID);
+		var value = form[key].value;
+		return value;
 	}
 }());
